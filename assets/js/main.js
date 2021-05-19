a418caa316f6c1074d7dbfd2cbac792cfd7b3d5c
linhareseduardo1 <70285912+linhareseduardo1@users.noreply.github.com>
2021-05-19 17:15:31 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -71,6 +71,23 @@
 								.addClass('active-locked');
 
 					})
+			function onReady(callback) {
+  						var intervalId = window.setInterval(function() {
+   						 if (document.getElementsByTagName('body')[0] !== undefined) {
+   						   window.clearInterval(intervalId);
+    						  callback.call(this);
+						    }
+						  }, 1000);
+						}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
 					.each(function() {
 
 						var	$this = $(this),
